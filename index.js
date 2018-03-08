164fabd1dce9a6258cedfefca74c27d551cfc7e4
Taylor LaRiviere <taylor.lariviere@gmail.com>
2018-03-08 11:58:42 -0700

    history attempt 1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 var clients = [];
-var history = [];
+var history = new CircularBuffer(200);
 var incr = 1;
 
 function getUsers() {
@@ -24,6 +24,7 @@ io.on('connection', function (socket) {
 
     socket.on('start', function () {
         socket.emit('nick', "guest" + incr);
+        socet.emit('history', history.toarray())
         clients[clients.indexOf(socket)].n = "guest" + incr;
         incr++;
         io.emit('users', getUsers());
@@ -31,6 +32,7 @@ io.on('connection', function (socket) {
 
     socket.on('send chat message', function (msg) {
         io.emit('chat message', msg);
+        history.enq(msg)
     });
 
     socket.on('set nick', function (nick) {
@@ -52,3 +54,61 @@ io.on('connection', function (socket) {
 http.listen(port, function () {
     console.log('listening on *:' + port);
 });
+
+// from https://github.com/tomsmeding/circular-buffer/blob/master/index.js
+CircularBuffer.prototype = {
+    size: function () { return this._size; },
+    capacity: function () { return this._capacity; },
+    enq: function (value) {
+        if (this._first > 0) this._first--; else this._first = this._capacity - 1;
+        this._buffer[this._first] = value;
+        if (this._size < this._capacity) this._size++;
+    },
+    push: function (value) {
+        if (this._size == this._capacity) {
+            this._buffer[this._first] = value;
+            this._first = (this._first + 1) % this._capacity;
+        } else {
+            this._buffer[(this._first + this._size) % this._capacity] = value;
+            this._size++;
+        }
+    },
+    deq: function () {
+        if (this._size == 0) throw new RangeError("dequeue on empty buffer");
+        var value = this._buffer[(this._first + this._size - 1) % this._capacity];
+        this._size--;
+        return value;
+    },
+    pop: function () { return this.deq(); },
+    shift: function () {
+        if (this._size == 0) throw new RangeError("shift on empty buffer");
+        var value = this._buffer[this._first];
+        if (this._first == this._capacity - 1) this._first = 0; else this._first++;
+        this._size--;
+        return value;
+    },
+    get: function (start, end) {
+        if (this._size == 0 && start == 0 && (end == undefined || end == 0)) return [];
+        if (typeof start != "number" || start % 1 != 0 || start < 0) throw new TypeError("Invalid start");
+        if (start >= this._size) throw new RangeError("Index past end of buffer: " + start);
+
+        if (end == undefined) return this._buffer[(this._first + start) % this._capacity];
+
+        if (typeof end != "number" || end % 1 != 0 || end < 0) throw new TypeError("Invalid end");
+        if (end >= this._size) throw new RangeError("Index past end of buffer: " + end);
+
+        if (this._first + start >= this._capacity) {
+            //make sure first+start and first+end are in a normal range
+            start -= this._capacity; //becomes a negative number
+            end -= this._capacity;
+        }
+        if (this._first + end < this._capacity)
+            return this._buffer.slice(this._first + start, this._first + end + 1);
+        else
+            return this._buffer.slice(this._first + start, this._capacity).concat(this._buffer.slice(0, this._first + end + 1 - this._capacity));
+    },
+    toarray: function () {
+        if (this._size == 0) return [];
+        return this.get(0, this._size - 1);
+    }
+};
